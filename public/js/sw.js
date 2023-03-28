4d70b2e19b9cf3404366c54770bffe4a67be27d5
Wellfix <dbrozyna15@gmail.com>
2023-03-28 04:32:37 +0200

    sw.js edit

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,50 +1,61 @@
-self.addEventListener("install", function(event) {
-    event.waitUntil(preLoad());
-});
+var GHPATH = '/pwa-pms';
+var APP_PREFIX = 'pwapms_';
+var VERSION = 'version_008';
+var URLS = [
+    `${GHPATH}/`,
+    `${GHPATH}/index.html`,
+    `${GHPATH}/offline.html`,
+    `${GHPATH}/mapa.html`,
+    `${GHPATH}/sos.html`,
+    `${GHPATH}/assets/icons/24x24.png`,
+    `${GHPATH}/assets/icons/48x48.png`,
+    `${GHPATH}/assets/icons/192x192.png`,
+    `${GHPATH}/assets/icons/512x512.png`,
+    `${GHPATH}/assets/icons/icon_24.png`,
+    `${GHPATH}/assets/icons/icon_48.png`,
+    `${GHPATH}/assets/icons/icon_192.png`,
+    `${GHPATH}/assets/icons/icon_512.png`,
+    `${GHPATH}/js/app.js`
+]
 
-var preLoad = function(){
-    console.log("Installing web app");
-    return caches.open("offline").then(function(cache) {
-        console.log("caching index and important routes");
-    });
-};
-
-self.addEventListener("fetch", function(event) {
-    event.respondWith(checkResponse(event.request).catch(function() {
-        return returnFromCache(event.request);
-    }));
-    event.waitUntil(addToCache(event.request));
-});
-
-var checkResponse = function(request){
-    return new Promise(function(fulfill, reject) {
-        fetch(request).then(function(response){
-            if(response.status !== 404) {
-                fulfill(response);
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+    console.log('Fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('Responding with cache : ' + e.request.url);
+                return request
             } else {
-                reject();
+                console.log('File is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
             }
-        }, reject);
-    });
-};
+        })
+    )
+})
 
-var addToCache = function(request){
-    return caches.open("offline").then(function (cache) {
-        return fetch(request).then(function (response) {
-            console.log(response.url + " was cached");
-            return cache.put(request, response);
-        });
-    });
-};
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('Installing cache : ' + CACHE_NAME);
+            return cache.addAll(URLS)
+        })
+    )
+})
 
-var returnFromCache = function(request){
-    return caches.open("offline").then(function (cache) {
-        return cache.match(request).then(function (matching) {
-            if(!matching || matching.status == 404) {
-                return cache.match("offline.html");
-            } else {
-                return matching;
-            }
-        });
-    });
-};
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheWhitelist.push(CACHE_NAME);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('Deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
